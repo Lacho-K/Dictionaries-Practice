020e45786ad144855fe7904597802b18008f4573
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 16:16:17 +0300

    Add files via upload

---
+++ dictStuff/dictStuff/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wardrobe
+{
+    class Wardrobe
+    {
+        static void Main(string[] args)
+        {            
+            int countNums = int.Parse(Console.ReadLine());
+            Dictionary<int, int> numbersInfo = new Dictionary<int, int>();
+
+            for (int i = 0; i < countNums; i++)
+            {
+                int curNum = int.Parse(Console.ReadLine());
+                if (!numbersInfo.ContainsKey(curNum))
+                {
+                    numbersInfo.Add(curNum, 1);
+                }
+                else
+                {
+                    numbersInfo[curNum] += 1;
+                }               
+            }
+
+            foreach (var item in numbersInfo)
+            {
+                if(item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+
+        }
+    }
+}
