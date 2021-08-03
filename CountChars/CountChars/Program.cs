370af3871092405d146e885d3b9da1228624b8c0
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-03 17:10:34 +0300

    Add files via upload

---
+++ CountChars/CountChars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            string[] input = Console.ReadLine().Split();
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    if (!charCount.ContainsKey(input[i][j]))
+                    {
+                        charCount.Add(input[i][j], 1);
+                    }
+                    else
+                    {
+                        charCount[input[i][j]] += 1;
+                    }
+                }
+            }
+
+            foreach (var item in charCount)
+            {
+                Console.WriteLine(item.Key + "->" + item.Value);
+            }
+        }
+    }
+}
