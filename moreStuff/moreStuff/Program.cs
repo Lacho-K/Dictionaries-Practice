544039f8108272ab2bcde58715bca7199cb73121
Lacho123 <44599728+Lacho123@users.noreply.github.com>
2021-08-04 12:31:40 +0300

    Add files via upload

---
+++ moreStuff/moreStuff/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace moreStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            SortedDictionary<string, string> dict = new SortedDictionary<string, string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] input = Console.ReadLine().Split(" | ");
+                dict.Add(input[1], input[0]);
+            }
+            string[] letterRestrict = Console.ReadLine().Split(" - ");
+            char startLetter = char.Parse(letterRestrict[0]);
+            char endLetter = char.Parse(letterRestrict[1]);         
+
+            foreach (var item in dict.Where(x => x.Key.First() > startLetter && x.Key.First() < endLetter))
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+    }
+}
